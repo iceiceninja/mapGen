dbbfc84f96cf52263c1880a5aec362155c9319ee
iceiceninja <joshdbonham@gmail.com>
2022-05-16 15:41:28 -0500

    Correctly implemented Lsystem (I think). Fixed minor bugs and added desert tile.

--- index.js
+++ index.js
@@ -10,43 +10,85 @@ class LSystem{
     }
     calculate(axiom, productionRules,iterations){
         let newAxiom = ""
+        console.log("Year: " + iterations)
+        if(iterations == 0) return axiom
         for(let i = 0; i<iterations; i++){
-            console.log(axiom)
+            newAxiom = ""
             for(let i = 0; i < axiom.length; i++){
+
                 newAxiom += productionRules[axiom.charAt(i)]
+                console.log("Character: ", axiom.charAt(i), ", prod rule: ",productionRules[axiom.charAt(i)])
+                if(newAxiom.length > 100) return newAxiom
             }
+            axiom = newAxiom
+            console.log(newAxiom)
+
         }
+       // console.log(newAxiom)
         return newAxiom
     }
     display(newAxiom){
+        console.log("display axiom: " + newAxiom)
         for(let i = 0; i < 100; i++){
+           // console.log("char at: ",i , ": ", newAxiom.charAt(i))
             MAP.innerHTML += "<div class=\"square\" name = \"" + newAxiom.charAt(i) + "\">" + 
             newAxiom.charAt(i) + "</div>"
         }
+        let landTiles = document.querySelectorAll("div[name=\"L\"")
+
+        let waterTiles = document.querySelectorAll("div[name=\"W\"")
+
+        let mountainTiles = document.querySelectorAll("div[name=\"M\"")
+
+        let riverTiles = document.querySelectorAll("div[name=\"R\"")
+
+        let desertTiles = document.querySelectorAll("div[name=\"D\"")
+
+        for(let i = 0; i < landTiles.length; i++){
+         landTiles[i].style.backgroundColor = "green"
+        }
+        for(let i = 0; i < waterTiles.length; i++){
+            waterTiles[i].style.backgroundColor = "blue"
+        }
+        for(let i = 0; i < mountainTiles.length; i++){
+            mountainTiles[i].style.backgroundColor = "brown"
+        }
+        for(let i = 0; i < riverTiles.length; i++){
+            riverTiles[i].style.backgroundColor = "cyan"
+        }
+        for(let i = 0; i < desertTiles.length; i++){
+            desertTiles[i].style.backgroundColor = "tan"
+        }
     }
 }
+
 productionRules = {
-    "L" : "LLWLL",
-    "W" : "WL",
-    "M" : "LMLWM"
+    "L" : "RL",//land
+    "W" : "MWW",//water
+    "M" : "LML",//mountain
+    "R" : "RR", //river
+    "D" : "LDD"//desert
 }
 
-axiom = "LWL"
-test = new LSystem("LMWL",productionRules)
-test.display(test.calculate(axiom,productionRules,50))
+axiom = "WWW"
+test = new LSystem(axiom,productionRules)
+iterations = 0
 
 
-landTiles = document.querySelectorAll("div[name=\"L\"")
-
-waterTiles = document.querySelectorAll("div[name=\"W\"")
-
-mountainTiles = document.querySelectorAll("div[name=\"M\"")
-for(let i = 0; i < landTiles.length; i++){
-    landTiles[i].style.backgroundColor = "green"
+function next(){
+    MAP.innerHTML = ""
+    iterations += 1
+    test.display(test.calculate(axiom,productionRules,iterations))
+    console.log(iterations)
 }
-for(let i = 0; i < waterTiles.length; i++){
-    waterTiles[i].style.backgroundColor = "blue"
+function last(){
+    MAP.innerHTML = ""
+    iterations -= 1
+    test.display(test.calculate(axiom,productionRules,iterations))
+    console.log(iterations)
 }
-for(let i = 0; i < mountainTiles.length; i++){
-    mountainTiles[i].style.backgroundColor = "brown"
+function submit(){
+    axiom = document.getElementById("textbox").value
+    console.log(axiom)
+
 }
